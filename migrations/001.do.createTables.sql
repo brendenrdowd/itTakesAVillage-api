4537ef2db20e6b7b172afb20501fb660f27c722f
Brenden Dowd <brenden.dowd@yahoo.com>
2020-05-09 21:46:09 -0700

    first

---
+++ migrations/001.do.createTables.sql
@@ -0,0 +1,38 @@
+CREATE TABLE IF NOT EXISTS itav_comments (
+  id SERIAL PRIMARY KEY,
+  comment TEXT,
+  created_at TIMESTAMP NOT NULL DEFAULT now()
+);
+
+CREATE TABLE IF NOT EXISTS itav_users (
+  id SERIAL PRIMARY KEY,
+  email TEXT NOT NULL UNIQUE, 
+  name TEXT NOT NULL, 
+  username TEXT NOT NULL UNIQUE,
+  password TEXT NOT NULL, 
+  location TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS  itav_stories (
+  id SERIAL PRIMARY KEY,
+  issue TEXT NOT NULL, 
+  keyword TEXT NOT NULL, 
+  resolved BOOLEAN DEFAULT FALSE,
+  created_at TIMESTAMP NOT NULL DEFAULT now()
+);
+
+ALTER TABLE itav_stories
+  ADD COLUMN
+    author INTEGER REFERENCES itav_users(id)
+    ON DELETE SET NULL;
+
+ALTER TABLE itav_comments
+  ADD COLUMN
+    author INTEGER REFERENCES itav_users(id)
+    ON DELETE SET NULL;
+
+ALTER TABLE itav_comments
+  ADD COLUMN
+    story INTERGER REFERENCES itav_stories(id)
+    ON DELETE SET NULL;
+
