2872970f62600beefa9e2453bd66272966820929
anotherchester <anotherchester@gmail.com>
2020-05-14 20:38:16 -0700

    endpoints fixed

--- src/comments/comments-service.js
+++ src/comments/comments-service.js
@@ -1,24 +1,24 @@
 const CommentsService = {
   getAllComments(knex) {
-    return knex.select('*').from('comment');
+    return knex.select('*').from('itav_comments');
   },
   getById(knex, id) {
-    return knex.from('comment').select('*').where({id}).first();
+    return knex.from('itav_comments').select('*').where({id}).first();
   },
   insertComment(knex, newComment) {
     return knex
     .insert(newComment)
-    .into('comment')
+    .into('itav_comments')
     .returning('*')
     .then((rows) => {
       return rows [0];
     });
   },
   deleteComment(knex, id) {
-    return knex('comment').where({id}).delete();
+    return knex('itav_comments').where({id}).delete();
   },
   editComment(knex, id, newComment) { //I'm not sure about proper name on this one!!!
-    return knex('comment').where({id}).update(newComment);
+    return knex('itav_comments').where({id}).update(newComment);
   },
 }
 
