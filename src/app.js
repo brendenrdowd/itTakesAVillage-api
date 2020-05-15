47be8d3708f3f2451bd92efdc589395da2380d3a
Rupali <rupalirajput@gmail.com>
2020-05-14 21:55:48 -0500

    user and auth router and service created and is working

--- src/app.js
+++ src/app.js
@@ -1,32 +1,35 @@
-require('dotenv').config()
-const express = require('express'),
-  morgan = require('morgan'),
-  cors = require('cors'),
-  helmet = require('helmet'),
-  app = express();
+require("dotenv").config();
+const express = require("express"),
+  morgan = require("morgan"),
+  cors = require("cors"),
+  helmet = require("helmet"),
+  app = express(),
+  { NODE_ENV } = require("./config"),
+  authRouter = require("./auth/auth-router"),
+  usersRouter = require("./users/users-router");
 
-const { NODE_ENV } = require('./config')
-const authRouter = require('./auth/auth-router')
+const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
 
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors())
+app.use("/api/auth", authRouter);
+app.use("/api/users", usersRouter);
 
-app.use('/api/auth', authRouter)
+app.get("/", (req, res) => {
+  res.send("Welcome to the GT Pro API!");
+});
 
 app.use(function errorHandler(error, req, res, next) {
-  let response
-  if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } }
+  let response;
+  if (NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
   } else {
-    console.error(error)
-    response = { message: error.message, error }
+    console.error(error);
+    response = { message: error.message, error };
   }
-  res.status(500).json(response)
-})
+  res.status(500).json(response);
+});
 
-module.exports = app
+module.exports = app;
--- src/users/users-service.js
+++ src/users/users-service.js
@@ -1,14 +1,23 @@
 const bcrypt = require("bcryptjs");
 const xss = require("xss");
 
-const REGEX_UPPER_LOWER_NUMBER_SPECIAL = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/;
+const REGEX_UPPER_LOWER_NUMBER_SPECIAL = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])/;
 
 const UsersService = {
     hasUserWithUserName(db, username) {
         return db("itav_users")
             .where({ username })
             .first()
-            .then(user => !!user);
+            .then((user) => !!user);
+    },
+    hasUserWithEmail(db, email) {
+        return db("itav_users")
+            .where({ email })
+            .first()
+            .then((email) => !!email);
+    },
+    getAllUsers(db) {
+        return db.from("itav_users").select("*");
     },
     insertUser(db, newUser) {
         return db
@@ -28,7 +37,7 @@ const UsersService = {
             return "Password must not start or end with empty spaces";
         }
         if (!REGEX_UPPER_LOWER_NUMBER_SPECIAL.test(password)) {
-            return "Password must contain one upper case, lower case, number and special character";
+            return "Password must contain at least one upper case letter";
         }
         return null;
     },
@@ -38,12 +47,13 @@ const UsersService = {
     serializeUser(user) {
         return {
             id: user.id,
-            full_name: xss(user.full_name),
+            name: xss(user.name),
             username: xss(user.username),
-            nickname: xss(user.nick_name),
-            date_created: new Date(user.date_created)
+            email: xss(user.email),
+            location: user.location,
+            password: user.password,
         };
-    }
+    },
 };
 
 module.exports = UsersService;
