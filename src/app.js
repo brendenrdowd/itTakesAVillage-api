15b5cd98c994d2b4fe280982f51887f27d85bb84
Brenden Dowd <brenden.dowd@yahoo.com>
2020-05-16 12:42:43 -0700

    updated master to coordinate multiple merges

--- src/app.js
+++ src/app.js
@@ -4,10 +4,10 @@ const express = require("express"),
   cors = require("cors"),
   helmet = require("helmet"),
   app = express(),
-  usersRouter = require("./user/users-router"),
   { NODE_ENV } = require("./config");
-
-// const authRouter = require("./auth/auth-router");
+  
+const usersRouter = require("./user/users-router");
+const authRouter = require("./auth/auth-router");
 const StoryRouter = require("./story/story-router");
 
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
@@ -16,9 +16,8 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-// app.use("/api/auth", authRouter);
-// left / off at the end on purpose
-app.use("/api", StoryRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/story", StoryRouter);
 app.use("/api/users", usersRouter);
 
 app.use(function errorHandler(error, req, res, next) {
--- src/server.js
+++ src/server.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-const app = require('./app');
-const { PORT, DATABASE_URL } = require('./config');
-
-const db = knex({
-  client: 'pg',
-  connection: DATABASE_URL,
-})
-
-app.set('db', db)
-=======
 const app = require("./app");
 const knex = require("knex");
 const { PORT, DATABASE_URL } = require("./config");
@@ -19,7 +8,6 @@ const db = knex({
 });
 
 app.set("db", db);
->>>>>>> story-route
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
