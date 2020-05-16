f41bd11880900845503f43664f373d29915f1167
Fernando Gaol <ffilho@riolindo.org>
2020-05-15 21:39:23 -0600

    no changes made

--- src/app.js
+++ src/app.js
@@ -1,32 +1,35 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express'),
   morgan = require('morgan'),
   cors = require('cors'),
   helmet = require('helmet'),
+  usersRouter = require('./')
   app = express();
 
-const { NODE_ENV } = require('./config')
-const authRouter = require('./auth/auth-router')
+const { NODE_ENV } = require('./config');
+const authRouter = require('./auth/auth-router');
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
+const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors())
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
 
-app.use('/api/auth', authRouter)
+app.use('/api/auth', authRouter);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the GT Pro API!');
+});
 
 app.use(function errorHandler(error, req, res, next) {
-  let response
+  let response;
   if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } }
+    response = { error: { message: 'server error' } };
   } else {
-    console.error(error)
-    response = { message: error.message, error }
+    console.error(error);
+    response = { message: error.message, error };
   }
-  res.status(500).json(response)
-})
+  res.status(500).json(response);
+});
 
-module.exports = app
+module.exports = app;
