d8c91e251a2d919238558fe838a93bdc08267a41
Rupali <rupalirajput@gmail.com>
2020-05-16 14:29:55 -0500

    resolved merging issues

--- src/config.js
+++ src/config.js
@@ -1,12 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-<<<<<<< HEAD
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL:"postgresql://postgres@localhost/village"
+  DATABASE_URL: "postgresql://postgres@localhost/village"
 }
-=======
-  NODE_ENV: process.env.NODE_ENV || "development",
-  DATABASE_URL: process.env.DATABASE_URL || "postgresql://postgres@localhost/village",
-    
-};
->>>>>>> story-route
