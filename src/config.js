301f2a86f156732581494c9cbeedf338bad2d5f8
Brenden Dowd <brenden.dowd@yahoo.com>
2020-05-19 18:54:36 -0700

    updated db strings

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: "postgresql://postgres@localhost/village"
+  DATABASE_URL: process.env.TEST_DATABASE_URL || "postgresql://postgres@localhost/village",
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/village-test'
+
 }
