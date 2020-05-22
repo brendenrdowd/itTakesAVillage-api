84af6b6040888ea1cf81b62a04c4710f574f74f8
Brenden Dowd <brenden.dowd@yahoo.com>
2020-05-21 20:20:24 -0700

    updated knex, added log

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const app = require("./app");
 const knex = require("knex");
 const { PORT, DATABASE_URL } = require("./config");
 
+console.log("db server url: ",DATABASE_URL)
 const db = knex({
   client: "pg",
   connection: DATABASE_URL,
